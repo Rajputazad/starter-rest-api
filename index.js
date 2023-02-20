e76e944204cc01ce2063a7d9c2025b731fd103b7
Your Name <you@example.com>
2023-02-20 14:55:22 +0530

    gerg

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const router = express.Router()
 const cors = require("cors")
 app.use(
     cors({
-      origin: ["http://localhost:4200", "*"],
+      origin: ["http://wbpanel.epizy.com/"],
       credentials: true,
     })
   );const cookies = require("cookie-parser")
@@ -17,7 +17,7 @@ const Login=require("./Login")(router)
 app.use((express.json({limit:"50mb"})))
 const morgan = require("morgan");
 app.use((res,req,next)=>{
-    res.header("Access-Control-Allow-Origin","http://localhost:4200")
+    res.header("Access-Control-Allow-Origin","http://wbpanel.epizy.com/")
     res.header("Access-Control-Allow-Methods","GET,POST,PUT,PATCH,HEAD,DELETE")
     res.header("Access-Control-Allow-Headers","authentication,Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id")
     next()
